d3fd960037d8f767fb5ab410ece440e0d7e77835
maxwen <max.weninger@gmail.com>
2019-10-23 13:22:01 +0200

    Launcher3: send device profile changes to all apps views
    now that there is a separate numColumnsAllApps we need to
    make sure device profile changes change that grid too
    
    also add a 6x6 grid device profile while we are here
    
    Change-Id: Ib4d0e06ed29b88481bbaad989f42665fc1db7032

--- src/com/android/launcher3/allapps/AllAppsRecyclerView.java
+++ src/com/android/launcher3/allapps/AllAppsRecyclerView.java
@@ -50,7 +50,7 @@ public class AllAppsRecyclerView extends BaseRecyclerView implements LogContaine
 
     private AlphabeticalAppsList mApps;
     private AllAppsFastScrollHelper mFastScrollHelper;
-    private final int mNumAppsPerRow;
+    private int mNumAppsPerRow;
 
     // The specific view heights that we use to calculate scroll
     private SparseIntArray mViewHeights = new SparseIntArray();
@@ -426,5 +426,19 @@ public class AllAppsRecyclerView extends BaseRecyclerView implements LogContaine
     @Override
     public boolean hasOverlappingRendering() {
         return false;
+    }
+
+    @Override
+    public void onScrollStateChanged(int state) {
+        super.onScrollStateChanged(state);
+
+        if (state == SCROLL_STATE_IDLE) {
+            AccessibilityManagerCompat.sendScrollFinishedEventToTest(getContext());
+        }
+    }
+
+    public void onDeviceProfileChanged(int appsPerRow) {
+        mNumAppsPerRow = appsPerRow;
+        updatePoolSize();
     }
 }
